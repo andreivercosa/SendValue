5596f3cced38337fa5aa2218fc70f12cceae7a5c
andreivercosa <andreivercosa@gmail.com>
2019-03-12 20:18:57 -0400

    v1

---
+++ SendValue/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SendValue
+{
+    public partial class MainPage : ContentPage
+    {
+        void calcular(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Result(Num1.Text, Num2.Text));
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SendValue/Result.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SendValue
+{
+    public partial class Result : ContentPage
+    {
+
+        public Result(string Num1, string Num2)
+        {
+            InitializeComponent();
+            int resutado = Convert.ToInt32(Num1) + Convert.ToInt32(Num2);
+            result.Text = "Resultado da soma: " + resutado;
+        }
+        void VoltarTela(object sender, System.EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
